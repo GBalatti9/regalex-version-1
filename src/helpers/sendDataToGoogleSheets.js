03b0f2ba5bdef2754b37e533609c9f2fd14e8f15
GBalatti9 <gas.balatti@gmail.com>
2024-11-28 09:52:39 -0300

    adding one question

--- src/helpers/sendDataToGoogleSheets.js
+++ src/helpers/sendDataToGoogleSheets.js
@@ -1,6 +1,6 @@
 export const sendDataToGoogleSheets = async (data) => {
     try {
-        const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw9hZ7JpL95852hkUdqZiE6KtBRW1lTkhMjNsNuwVjuhp2i7NKpKrdF0oulFdYf9A_f/exec';
+        const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbziFK66QG9OsHzkrf_f-MjC_awbZj6pdUcXG3EL5Ab8KZMEyEAU1BlVDOYoS25fojmz/exec';
         
         // Send raw JSON instead of form data
         const response = await fetch(GOOGLE_APPS_SCRIPT_URL, {
